0e7641fd21013d310d3b8b1696bbeb1084ec15cd
luistv <luistv@luis-HP-1000>
2014-06-10 13:54:44 -0500

    1 >> Inicio de Proyecto 2 >> Agregando funciones a Estudiante

--- src/Clases/Estudiante.java
+++ src/Clases/Estudiante.java
@@ -11,8 +11,48 @@ package Clases;
  * @author luistv
  */
 public class Estudiante {
-
-    public Estudiante() {
-    }
+    String Nombres;
+    String Apellidos;
+    String user;
+    String contrase;
     
+
+    public String getNombres() {
+        return Nombres;
+    }
+
+    public void setNombres(String Nombres) {
+        this.Nombres = Nombres;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public void setApellidos(String Apellidos) {
+        this.Apellidos = Apellidos;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getContrase() {
+        return contrase;
+    }
+
+    public void setContrase(String contrase) {
+        this.contrase = contrase;
+    }
+
+    public Estudiante(String Nombres, String Apellidos, String user, String contrase) {
+        this.Nombres = Nombres;
+        this.Apellidos = Apellidos;
+        this.user = user;
+        this.contrase = contrase;
+    }
 }
